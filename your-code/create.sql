10fd7134efec73aa4ec5bb0ec4f425497271a1bd
gi2302 <ginosca23@gmail.com>
2025-01-02 23:20:19 -0400

    Add files via upload

--- your-code/create.sql
+++ your-code/create.sql
@@ -14,7 +14,7 @@ CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    phone VARCHAR(15),
+    phone VARCHAR(20),
     email VARCHAR(100),
     address VARCHAR(255),
     city VARCHAR(50),
--- your-code/seeding.sql
+++ your-code/seeding.sql
@@ -1,32 +1,35 @@
-insert into cars(car_id, manufacturer, model, year_, color)
-values ('0', 'Volkswagen','Tiguan', '2019','Blue'),
-('1', 'Peugeot','Rifter', '2019', 'Red'),
-('2', 'Ford','Fusion', '2018', 'White'),
-('3', 'Toyota','RAV4', '2018', 'Silver'),
-('4', 'Volvo', 'V60', '2019', 'Gray'),
-('5', 'Volvo','V60 Cross Country', '2019', 'Gray');
+-- Use the database
+USE lab_mysql;
 
+-- Insert dummy data into the Cars table
+INSERT INTO Cars (VIN, manufacturer, model, year, color) VALUES
+('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
+('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
+('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
+('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
+('DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray'),
+('DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
 
-insert into costumer(costumer_id, first_name, phone_no, email, address, city, state, country, zip_code)
-values ('10001', 'Pablo Picasso', '+34 636 17 63 82', '-', 'Paseo de la Chope', 'Madrid', 'Madrid', 'Spain', '28045'),
-('20001', 'Abraham Lincoln', '+1 305 907 7086', '-', '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
-('30001', 'Napoleon Bonaparte', '+33 1 79 75 40 00', '-', '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
+-- Insert dummy data into the Customers table
+INSERT INTO Customers (first_name, last_name, phone, email, address, city, state_province, country, zip_code) VALUES
+('Pablo', 'Picasso', '+34 636 17 63 82', NULL, 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+('Abraham', 'Lincoln', '+1 305 907 7086', NULL, '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
+('Napoléon', 'Bonaparte', '+33 1 79 75 40 00', NULL, '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
 
+-- Insert dummy data into the Salespersons table
+INSERT INTO Salespersons (name, store) VALUES
+('Petey Cruiser', 'Madrid'),
+('Anna Sthesia', 'Barcelona'),
+('Paul Molive', 'Berlin'),
+('Gail Forcewind', 'Paris'),
+('Paige Turner', 'Mimia'),
+('Bob Frapples', 'Mexico City'),
+('Walter Melon', 'Amsterdam'),
+('Shonda Leer', 'São Paulo');
 
-insert into salesperson (salesperson_id, first_name, store_name)
-values ('00001', 'Petey Cruiser', '0'),
-('00002', 'Anna Sthesia', '1'),
-('00003', 'Paul Molive', '2'),
-('00004', 'Gail Forcewind', '3'),
-('00005', 'Paige Turner', '4'),
-('00006', 'Bob Frapples', '5'),
-('00007', 'Walter Melon', '6'),
-('00008', 'Shonda Leer', 'São Paulo');
-
-insert into invoices (invoice_id, date_, car, costumer, salesperson)
-values ('852399038', '2018-08-22',	'0','1','3'),
-('731166526', '2018-12-31', '3', '0', '5'),
-('271135104', '2019-01-22', '2','2','7');
-
-
+-- Insert dummy data into the Invoices table
+INSERT INTO Invoices (date, car_id, customer_id, salesperson_id) VALUES
+('2018-08-22', 1, 2, 4),
+('2018-12-31', 4, 1, 6),
+('2019-01-22', 3, 3, 8);
 
